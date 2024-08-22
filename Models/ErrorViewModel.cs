61681275a7b7f7d1f07958ffd01375dfda78c7cd
LEILA ALUMNE ENTENZA CHIDERSKI <48385438@ort>
2024-08-22 13:33:31 -0300

    proyecto creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP7_PreguntadORT_Entenza_Zilbersztein.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
